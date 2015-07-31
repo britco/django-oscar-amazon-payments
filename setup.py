bbc38eb9c458517c8e418da138b40121f2aeb18b
Stephane Jolicoeur <s.jolicoeur@gmail.com>
2015-07-31 14:28:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-oscar-amazon-payments',
-    version="0.2.2",
+    version="0.2.3",
     url='https://github.com/simonkagwe/django-oscar-amazon-payments',
     author="Simon Kagwi",
     author_email="simonkagwe@yahoo.com",
